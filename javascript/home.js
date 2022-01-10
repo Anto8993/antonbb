c12cad3f9867d1b4db3a4be764ecd27c53e52d2d
Anto8993 <anto8993@stud.kea.dk>
2022-01-10 09:57:37 +0100

    Jeg tror sgu jeg er færdig

--- javascript/home.js
+++ javascript/home.js
@@ -11,4 +11,38 @@
         document.documentElement.style.setProperty("--x", x);
         document.documentElement.style.setProperty("--y", y);
         raf = null;
-      }
+      };
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = "1";
+   
+  } else {
+    mybutton.style.opacity = "0";
+   
+
+  }
+
+
+
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
