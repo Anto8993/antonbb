708ff91f196e12b28d2426eef86b6ba5c3906d39
Anto8993 <anto8993@stud.kea.dk>
2022-01-11 11:55:02 +0100

    button cursor og meta name

--- javascript/home.js
+++ javascript/home.js
@@ -21,17 +21,15 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction();};
 
 function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.opacity = "1";
+    mybutton.style.cursor = "pointer";
+    
    
   } 
   else {
     mybutton.style.opacity = "0";
+    mybutton.style.cursor = "default";
   }
 }
 
